2df7c818a5f6d7c163b610a8d9371aa3806fe18c
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2022-09-21 18:00:48 +0100

    modified version -- no need to install xanesnet package

---
+++ structure/rdc.py
@@ -0,0 +1,122 @@
+"""
+XANESNET
+Copyright (C) 2021  Conor D. Rankine
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either Version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+############################### LIBRARY IMPORTS ###############################
+###############################################################################
+
+import numpy as np
+
+from ase import Atoms
+
+from structure.descriptors import Descriptor
+from structure.descriptors import VectorDescriptor
+
+###############################################################################
+################################## CLASSES ####################################
+###############################################################################
+
+class RDC(VectorDescriptor):
+    """
+    A class for transforming a molecular system into a radial (or 'pair')
+    distribution curve (RDCs). The RDC is - simplistically - like a histogram
+    of pairwise internuclear distances discretised over an auxilliary
+    real-space grid and smoothed out using Gaussians; pairs are made between
+    the absorption site and all atoms within a defined radial cutoff.
+    """
+
+    def __init__(
+        self, 
+        r_min: float = 0.0,
+        r_max: float = 8.0,
+        dr: float = 0.01,
+        alpha: float = 10.0,
+        use_charge = False,
+        use_spin = False
+    ):
+        """
+        Args:
+            r_min (float): The minimum radial cutoff distance (in A) around
+                the absorption site; should be 0.0.
+                Defaults to 0.0.
+            r_max (float): The maximum radial cutoff distance (in A) around
+                the absorption site.
+                Defaults to 6.0.
+            dr (float): The step size (in A) for the auxilliary real-space grid
+                that the RDC is discretised over.
+                Defaults to 0.01.
+            alpha (float): A smoothing parameter used in a Gaussian exponent
+                that defines the effective spatial resolution of the RDC.
+                Defaults to 10.0.
+            use_charge (bool): If True, includes an additional element in the
+                vector descriptor for the charge state of the complex.
+                Defaults to False.
+            use_spin (bool): If True, includes an additional element in the
+                vector descriptor for the spin state of the complex.
+                Defaults to False.
+        """
+
+        super().__init__(r_min, r_max, use_charge, use_spin)
+        
+        if isinstance(dr, (int, float)) and r_max >= dr > 0.0:
+            self.dr = float(dr)
+        else:
+            raise ValueError(f'expected dr: int/float > 0.0; got {dr}')
+
+        if isinstance(alpha, (int, float)) and alpha > 0.0:
+            self.alpha = float(alpha)
+        else:
+            raise ValueError(f'expected alpha: int/float > 0.0; got {alpha}')
+
+        nr_aux = int(np.absolute(self.r_max - self.r_min) / self.dr) + 1
+        self.r_aux = np.linspace(self.r_min, self.r_max, nr_aux)
+
+    def transform(self, system: Atoms) -> np.ndarray:
+        
+        if not isinstance(system, Atoms):
+            raise TypeError(f'systems passed as arguments to .transform ',
+                'should be ase.Atoms objects')
+
+        rij_in_range = system.get_distances(0, range(len(system))) < self.r_max
+        system = system[rij_in_range]
+        
+        ij = [[0,j] for j in range(1, len(system))]
+        if len(ij) < 1:
+            raise RuntimeError(f'too few atoms within {self.r_max:.2f} A of ',
+                'the absorption site to set up non-zero radial distribution ',
+                'curve (no pairs)')
+        else:
+            ij = np.array(ij, dtype = 'uint16')
+
+        zi = system.get_atomic_numbers()[ij[:,0]]
+        zj = system.get_atomic_numbers()[ij[:,1]]
+        rij = system.get_distances(ij[:,0], ij[:,1])
+        rij_r_sq = np.square(rij[:, np.newaxis] - self.r_aux)
+        exp = np.exp(-1.0 * self.alpha * rij_r_sq)
+        rdc = np.sum((zi * zj)[:, np.newaxis] * exp, axis = 0)
+
+        if self.use_spin:
+            rdc = np.append(system.info['S'], rdc)
+
+        if self.use_charge:
+            rdc = np.append(system.info['q'], rdc)
+
+        return rdc
+
+    def get_len(self) -> int:
+        
+        return len(self.r_aux) + self.use_charge + self.use_spin
---
+++ utils.py
@@ -0,0 +1,122 @@
+"""
+XANESNET
+Copyright (C) 2021  Conor D. Rankine
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either Version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+############################### LIBRARY IMPORTS ###############################
+###############################################################################
+
+import numpy as np
+
+from pathlib import Path
+
+###############################################################################
+################################## FUNCTIONS ##################################
+###############################################################################
+
+def unique_path(path: Path, base_name: str) -> Path:
+    # returns a unique path from `p`/`base_name`_001, `p`/`base_name`_002,
+    # `p`/`base_name`_003, etc.
+
+    n = 0
+    while True:
+        n += 1
+        unique_path = path / (base_name + f'_{n:03d}')
+        if not unique_path.exists():
+            return unique_path
+
+def linecount(f: Path) -> int:
+    # returns the linecount for a file (`f`)
+
+    with open(f, 'r') as f_:
+        return len([l for l in f_])
+
+def list_files(d: Path, with_ext: bool = True) -> list:
+    # returns a list of files (as POSIX paths) found in a directory (`d`);
+    # 'hidden' files are always omitted and, if with_ext == False, file
+    # extensions are also omitted
+
+    return [(f if with_ext else f.with_suffix('')) 
+        for f in d.iterdir() if f.is_file() and not f.stem.startswith('.')]
+
+def list_filestems(d: Path) -> list:
+    # returns a list of file stems (as strings) found in a directory (`d`);
+    # 'hidden' files are always omitted
+
+    return [f.stem for f in list_files(d)]
+
+def str_to_numeric(str_: str):
+    # returns the numeric (floating-point or integer) cast of `str_` if
+    # cast is allowed, otherwise returns `str_`
+
+    try:
+        return float(str_) if '.' in str_ else int(str_)
+    except ValueError:
+        return str_
+
+def print_nested_dict(dict_: dict, nested_level: int = 0):
+    # prints the key:value pairs in a dictionary (`dict`) in the format
+    # '>> key :: value'; iterates recursively through any subdictionaries,
+    # indenting with two white spaces for each sublevel (`nested level`)
+
+    for key, val in dict_.items():
+        if not isinstance(val, dict):
+            if isinstance(val, list):
+                val = f'[{val[0]}, ..., {val[-1]}]'
+            print('  ' * nested_level + f'>> {key} :: {val}')
+        else:
+            print('  ' * nested_level + f'>> {key}')
+            print_nested_dict(val, nested_level = nested_level + 1)
+
+    return 0
+
+def print_cross_validation_scores(scores: dict):
+    # prints a summary table of the scores from k-fold cross validation;
+    # summarises the elapsed time and train/test metric scores for each k-fold
+    # with overall k-fold cross validation statistics (mean and std. dev.)
+    # using the `scores` dictionary returned from `cross_validate`
+
+    print('')
+    print('>> summarising scores from k-fold cross validation...')
+    print('')
+
+    print('*' * 48)
+    
+    fmt = '{:<10s}{:>6s}{:>16s}{:>16s}'
+    print(fmt.format('k-fold', 'time', 'train', 'test'))
+    
+    print('*' * 48)
+
+    fmt = '{:<10.0f}{:>5.1f}s{:>16.8f}{:>16.8f}'
+    for kf, (t, train, test) in enumerate(zip(
+        scores['fit_time'], scores['train_score'], scores['test_score'])):
+        print(fmt.format(kf, t, np.absolute(train), np.absolute(test)))
+
+    print('*' * 48)
+
+    fmt = '{:<10s}{:>5.1f}s{:>16.8f}{:>16.8f}'
+    means_ = (np.mean(np.absolute(scores[score])) 
+        for score in ('fit_time', 'train_score', 'test_score'))
+    print(fmt.format('mean', *means_))
+    stdevs_ = (np.std(np.absolute(scores[score])) 
+        for score in ('fit_time', 'train_score', 'test_score'))
+    print(fmt.format('std. dev.', *stdevs_))
+
+    print('*' * 48)
+
+    print('')
+
+    return 0
