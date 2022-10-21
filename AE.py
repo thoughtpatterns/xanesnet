fbd1ab60a48bb3d78afc8850f401d84a3e19e67a
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2022-10-21 15:04:10 +0100

    the basic AE

---
+++ AE.py
@@ -0,0 +1,122 @@
+import torch
+from torch import nn, optim
+
+class AE (nn.Module):
+    def __init__(self, input_size, hidden_size, dropout, hl_shrink, out_dim):
+        super().__init__()
+    
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.dropout = dropout
+        self.hl_shrink = hl_shrink
+        self.out_dim = out_dim
+
+        self.encoder_hidden_1 = nn.Sequential(
+            nn.Linear(self.input_size, 256),
+            nn.PReLU()
+        )
+         
+        self.encoder_hidden_2 = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.PReLU()
+        )
+
+        self.encoder_output = nn.Sequential(
+            nn.Linear(128, 128),
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.Dropout(p=0.3),
+            nn.PReLU()
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(256, self.out_dim),
+            nn.PReLU(),
+        )
+
+        self.decoder_hidden_1 = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.PReLU()
+        )
+
+        self.decoder_hidden_2 = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.PReLU()
+        )
+
+        self.decoder_output = nn.Sequential(
+            nn.Linear(256, self.input_size)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        x = self.encoder_hidden_1(x)
+        x = self.encoder_hidden_2(x)
+        
+        latent_space = self.encoder_output(x)
+        pred_y = self.fc1(latent_space)
+        pred_y = self.fc2(pred_y)
+        
+        out = self.decoder_hidden_1(latent_space)
+        out = self.decoder_hidden_2(out)
+        recon = self.decoder_output(out)
+
+        return recon, pred_y
+
+
+def train_ae (x, y, hyperparams, n_epoch):
+
+    device  = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    out_dim = y[0].size
+    n_in = x.shape[1]
+   
+    x = torch.from_numpy(x)
+    y = torch.from_numpy(y)
+    
+    trainset = torch.utils.data.TensorDataset(x, y)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32)
+   
+    model = AE(n_in, 512, hyperparams['dropout'], hyperparams['hl_shrink'], out_dim)
+    model.to(device)
+    # mlp.apply(weight_init)
+    model.train()
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+    criterion = nn.MSELoss()
+    # criterion = nn.L1Loss()
+
+    for epoch in range(n_epoch):
+        running_loss = 0
+        running_recon = 0
+        running_pred = 0
+        for inputs, labels in trainloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, labels = inputs.float(), labels.float()
+            # print(inputs.shape)
+            optimizer.zero_grad()
+            recon_input, outputs = model(inputs)
+            
+
+            loss_recon = criterion(recon_input, inputs) 
+            loss_pred = criterion(outputs, labels)
+            loss = loss_recon + (1 * loss_pred)
+            # loss = custom_loss(logps, labels)
+            # loss = earth_mover_distance(labels, logps)
+            # print(loss.shape)
+            # loss.backward()
+            loss.mean().backward()
+
+            optimizer.step()
+            running_loss += loss.mean().item()
+            running_recon += loss_recon.mean().item()
+            running_pred += loss_pred.mean().item()
+            # print(loss.item())
+            
+        print("total loss:", running_loss/len(trainloader))
+        print("recon loss:", running_recon/len(trainloader))
+        print("pred loss:", running_pred/len(trainloader))
+
+
+    return epoch, model, optimizer
---
+++ ae_predict.py
@@ -0,0 +1,167 @@
+"""
+XANESNET
+Copyright (C) 2021  Conor D. Rankine
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either Version 3 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+############################### LIBRARY IMPORTS ###############################
+###############################################################################
+
+import numpy as np
+import pickle as pickle
+import tqdm as tqdm
+
+from pathlib import Path
+
+from inout import load_xyz
+# from inout import save_xyz
+from inout import load_xanes
+from inout import save_xanes
+# from inout import load_pipeline
+# from inout import save_pipeline
+from utils import unique_path
+from utils import list_filestems
+from utils import linecount
+from structure.rdc import RDC
+from structure.wacsf import WACSF
+from spectrum.xanes import XANES
+# from tensorflow.keras.models import model_from_json
+from mlp_pytorch import MLP
+import torch
+from sklearn.metrics import mean_squared_error
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pyemd import emd_samples
+
+###############################################################################
+################################ MAIN FUNCTION ################################
+###############################################################################
+ 
+def main(
+    model_dir: str,
+    x_path: str,
+    y_path:str
+):
+    """
+    PREDICT. The model state is restored from a model directory containing
+    serialised scaling/pipeline objects and the serialised model, .xyz (X)
+    data are loaded and transformed, and the model is used to predict XANES
+    spectral (Y) data; convolution of the Y data is also possible if
+    {conv_params} are provided (see xanesnet/convolute.py).
+
+    Args:
+        model_dir (str): The path to a model.[?] directory created by
+            the LEARN routine.
+        x_path (str): The path to the .xyz (X) data; expects a directory
+            containing .xyz files.
+    """
+
+    model_dir = Path(model_dir)
+
+    x_path = Path(x_path)
+    y_path = Path(y_path)
+
+    ids = list(
+            set(list_filestems(x_path)) & set(list_filestems(y_path))
+        )
+
+    ids.sort()
+
+    with open(model_dir / 'descriptor.pickle', 'rb') as f:
+        descriptor = pickle.load(f)
+
+    n_samples = len(ids)
+    n_x_features = descriptor.get_len()
+    n_y_features = linecount(y_path / f'{ids[0]}.txt') - 2
+
+    x = np.full((n_samples, n_x_features), np.nan)
+    print('>> preallocated {}x{} array for X data...'.format(*x.shape))
+    y = np.full((n_samples, n_y_features), np.nan)
+    print('>> preallocated {}x{} array for Y data...'.format(*y.shape))
+    print('>> ...everything preallocated!\n')
+
+    print('>> loading data into array(s)...')
+    for i, id_ in enumerate(tqdm.tqdm(ids)):
+        with open(x_path / f'{id_}.xyz', 'r') as f:
+            atoms = load_xyz(f)
+        x[i,:] = descriptor.transform(atoms)
+        with open(y_path / f'{id_}.txt', 'r') as f:
+            xanes = load_xanes(f)
+            # print(xanes.spectrum)
+            e, y[i,:] = xanes.spectrum
+    print('>> ...loaded!\n')
+
+
+    # pipeline = load_pipeline(
+    #     model_dir / 'net.keras',
+    #     model_dir / 'pipeline.pickle'
+    # )
+
+    # load the model
+    # model = MLP()
+    model_file = open(model_dir / 'model.pt', 'r')
+    # # loaded_model = torch.load(model_file)
+    # model.load_state_dict(torch.load(model_file))
+
+    model = torch.load(model_dir / 'model.pt', map_location=torch.device('cpu'))
+    model.eval()
+    print("Loaded model from disk")
+    # print(model)
+
+    x = torch.from_numpy(x)
+    x = x.float()
+
+    print('>> predicting Y data with neural net...')
+    x_recon, y_predict = model(x)
+    # if y_predict.ndim == 1:
+    #     if len(ids) == 1:
+    #         y_predict = y_predict.reshape(-1, y_predict.size)
+    #     else:
+    #         y_predict = y_predict.reshape(y_predict.size, -1)
+    print('>> ...predicted Y data!\n')
+
+    print(mean_squared_error(x, x_recon.detach().numpy()))
+    print(mean_squared_error(y, y_predict.detach().numpy()))
+    # print(emd_samples(y, y_predict.detach().numpy()))
+
+    predict_dir = unique_path(Path('.'), 'predictions')
+    predict_dir.mkdir()
+
+    # with open(model_dir / 'dataset.npz', 'rb') as f:
+    #     e = np.load(f)['e']
+
+    print('>> saving Y data predictions...')
+    for id_, y_predict_, y_, x_recon_, x_ in tqdm.tqdm(zip(ids, y_predict, y, x_recon, x)):
+        sns.set()
+        fig, (ax1, ax2) = plt.subplots(2)
+
+        ax1.plot(y_predict_.detach().numpy(), label="prediction")
+        ax1.set_title('prediction')
+        ax1.plot(y_, label="target")
+        ax1.legend(loc="upper right")
+        
+        ax2.plot(x_recon_.detach().numpy(), label="prediction")
+        ax2.set_title('reconstruction')
+        ax2.plot(x_, label="target")
+        ax2.legend(loc="upper right")
+
+        with open(predict_dir / f'{id_}.txt', 'w') as f:
+            # save_xanes(f, XANES(e, y_predict_.detach().numpy()))
+            plt.savefig(predict_dir / f'{id_}.pdf')
+        fig.clf()
+        plt.close(fig)
+    print('...saved!\n')
+        
+    return 0
