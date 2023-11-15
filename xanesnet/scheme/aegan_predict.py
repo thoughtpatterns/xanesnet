749bb7a4bc5f8dc8a82b8e190784bad0df5bf03e
Bowen Li <libowen96@hotmail.com>
2023-11-15 09:08:16 +0000

    Add Optuna section to the workflow input files; Update NN and AEGAN predictions

--- xanesnet/scheme/aegan_predict.py
+++ xanesnet/scheme/aegan_predict.py
@@ -14,6 +14,7 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import torch
+from sklearn.metrics import mean_squared_error
 
 from xanesnet.scheme.base_predict import Predict
 from xanesnet.data_transform import (
@@ -74,8 +75,27 @@ class AEGANPredict(Predict):
                 recon_xanes = self.model.reconstruct_spectrum(xanes)
                 pred_xyz = self.model.predict_structure(xanes)
 
+        return recon_xyz, pred_xanes, recon_xanes, pred_xyz
+
     def predict_bootstrap(self):
-        pass
+        xanes_pred_score = []
+        xyz_pred_score = []
+        xanes_recon_score = []
+        xyz_recon_score = []
+
+        xyz_recon, xanes_pred, xanes_recon, xyz_pred = self.predict()
+        if self.pred_mode == "predict_xyz" or self.pred_mode == "predict_all":
+            mse1 = mean_squared_error(self.xanes_data, xanes_recon.detach().numpy())
+            mse2 = mean_squared_error(self.xanes_data, xanes_pred.detach().numpy())
+
+            xanes_recon_score.append(mse1)
+            xanes_pred_score.append(mse2)
+        elif self.pred_mode == "predict_xanes" or self.pred_mode == "predict_all":
+            mse1 = mean_squared_error(self.xyz_data, xyz_recon.detach().numpy())
+            mse2 = mean_squared_error(self.xyz_data, xyz_pred.detach().numpy())
+
+            xanes_recon_score.append(mse1)
+            xanes_pred_score.append(mse2)
 
     def predict_ensemble(self):
         pass
