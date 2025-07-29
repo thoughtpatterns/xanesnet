924e1de23f2d32fafbeebdf7e9bd15bc2f53537c
Mohamad Makki <moe.m.makki@gmail.com>
2025-07-29 15:26:50 -0400

    reinit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Developer shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python310;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cmake
+            gfortran
+            llvmPackages.openmp
+            pkg-config
+            python
+            uv
+          ];
+
+          shellHook = ''
+            if ! [ -d .venv ]
+            then uv venv -p ${python}/bin/python
+            fi
+
+            unset VIRTUAL_ENV
+            . .venv/bin/activate
+          '';
+        };
+      }
+    );
+}
