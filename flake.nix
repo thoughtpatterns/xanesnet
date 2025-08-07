9e1214e0793dc20af119d57b5930e1738862b052
Mohamad Makki <moe.m.makki@gmail.com>
2025-08-06 23:02:54 -0400

    add cuda wsl support to flake

--- flake.nix
+++ flake.nix
@@ -15,11 +15,14 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true; # Required for `cudatoolkit`.
+        };
+
         python = pkgs.python310;
-      in
-      {
-        devShells.default = pkgs.mkShell {
+
+        commonArgs = {
           buildInputs = with pkgs; [
             cmake
             gfortran
@@ -29,7 +32,7 @@
             uv
           ];
 
-          NIX_CFLAGS_COMPILE = "-ftemplate-depth=2048"; # Needed for `dscribe`.
+          NIX_CFLAGS_COMPILE = "-ftemplate-depth=2048"; # Required for `dscribe`.
 
           shellHook = ''
             if ! [ -d .venv ]
@@ -38,7 +41,36 @@
 
             unset VIRTUAL_ENV
             . .venv/bin/activate
+
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:''${LD_LIBRARY_PATH-}"
           '';
+        };
+
+        cudaPackages = with pkgs; [
+          cudatoolkit_11
+          linuxPackages.nvidia_x11
+          ncurses5
+        ];
+
+        cudaShellHook =
+          commonArgs.shellHook
+          + ''
+            export CUDA_PATH="${pkgs.cudatoolkit_11}"
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath cudaPackages}:/usr/lib/wsl/lib:''${LD_LIBRARY_PATH-}"
+            export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+            export EXTRA_CCFLAGS="-I/usr/include"
+          '';
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell commonArgs;
+          cuda = pkgs.mkShell (
+            commonArgs
+            // {
+              buildInputs = commonArgs.buildInputs ++ cudaPackages;
+              shellHook = cudaShellHook;
+            }
+          );
         };
       }
     );
